7bd7ff594b642ed50126fe8e69a36d33dd35f9a5
Frank Xu <frankxu2004@gmail.com>
2021-09-29 00:35:22 -0400

    update context vector visualizations

--- extract_dstore_vecs.py
+++ extract_dstore_vecs.py
@@ -1,16 +1,32 @@
 import numpy as np
-import torch
 
-dictionary = torch.load('saved_tensors/wikitext-103/dictionary.pt')
+from fairseq.data import Dictionary
 
-keys = np.memmap('checkpoints/wikitext-103/validtrain_dstore_keys.npy', dtype=np.float16, shape=(91887387, 1024))
-vals = np.memmap('checkpoints/wikitext-103/validtrain_dstore_vals.npy',
-                 dtype=np.int, mode='r', shape=(91887387, 1))
+dictionary = Dictionary.load('data-bin/wikitext103-bpe/dict.txt')
+
+keys = np.memmap('checkpoints/wikitext103-bpe/dstore_keys.npy',
+                 dtype=np.float16, shape=(153225485, 512))
+vals = np.memmap('checkpoints/wikitext103-bpe/dstore_vals.npy',
+                 dtype=np.int, mode='r', shape=(153225485, 1))
+print(vals.dtype)
+
 vals = vals.squeeze()
 print(len(dictionary))
-token_id = dictionary.index('decide')
-filtered_keys = keys[vals == token_id, :]
 
-print(filtered_keys.shape)
+to_save = ['bank', 'shore', 'institution', 'beautiful']
+vectors = []
+labels = []
+for word in to_save:
+    token_id = dictionary.index(word)
+    filtered_keys = keys[vals == token_id, :]
+    print(filtered_keys.shape)
+    labels += [word] * filtered_keys.shape[0]
+    vectors.append(filtered_keys)
+    # np.savetxt('decide.txt', filtered_keys, delimiter='\t', fmt='%1.10f')
+all_vecs = np.concatenate(vectors, axis=0)
 
-np.savetxt('decide.txt', filtered_keys, delimiter='\t', fmt='%1.10f')
+np.savetxt('contextvecs.txt', all_vecs, delimiter='\t', fmt='%.15f')
+with open('labels.txt', 'w') as outfile:
+    for w in labels:
+        outfile.write(w + '\n')
+
