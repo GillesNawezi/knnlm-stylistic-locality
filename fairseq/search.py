348f4c9e66ae430e25cb9b9bc0021121e83d0e85
GillesNawezi <46746066+GillesNawezi@users.noreply.github.com>
2022-12-15 02:01:58 +0100

    Interactive Debug

--- fairseq/search.py
+++ fairseq/search.py
@@ -78,6 +78,9 @@ class BeamSearch(Search):
             ),
             out=(self.scores_buf, self.indices_buf),
         )
+        print(self.indices_buf)
+        print(vocab_size)
+        print(self.beams_buf)
         torch.div(self.indices_buf, vocab_size, out=self.beams_buf)
         self.indices_buf.fmod_(vocab_size)
         return self.scores_buf, self.indices_buf, self.beams_buf
